5ab04aaff94e4db93e0dc3337c99a1f3d8698876
Kohányi Róbert <kohanyi.robert@gmail.com>
2014-06-21 13:07:55 +0200

    Update test/misc_test.cpp.

--- test/misc_test.cpp
+++ test/misc_test.cpp
@@ -6,47 +6,61 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 TEST_CLASS(misc_test_class)
 {
-	public:
-		TEST_METHOD(misc_test)
+private:
+	std::vector<std::string const> new_vector()
+	{
+		std::vector<std::string const> vector;
+		vector.push_back("a");
+		vector.push_back("b");
+		vector.push_back("c");
+		vector.push_back("d");
+		vector.push_back("e");
+		return vector;
+	}
+
+	void assert_vectors(
+		std::vector<std::string const> const & vector_a,
+		std::vector<std::string const> const & vector_b,
+		bool const & expected)
+	{
+		Assert::AreEqual(
+			expected,
+			vector_a[0] == vector_b[0]
+		&& vector_a[1] == vector_b[1]
+		&& vector_a[2] == vector_b[2]
+		&& vector_a[3] == vector_b[3]
+		&& vector_a[4] == vector_b[4]);
+	}
+
+public:
+	TEST_METHOD(misc_test)
+	{
+		std::chrono::system_clock::time_point time_point_a(std::chrono::system_clock::now());
+
+		my::misc::sleep_milliseconds(1);
+
+		std::chrono::system_clock::time_point time_point_b(std::chrono::system_clock::now());
+
+		for (unsigned int i(0); i < 3; ++i)
 		{
-			std::vector<std::string const> vector_a;
-			vector_a.push_back("a");
-			vector_a.push_back("b");
-			vector_a.push_back("c");
-			vector_a.push_back("d");
-			vector_a.push_back("e");
-
-			std::vector<std::string const> vector_b;
-			vector_b.push_back("a");
-			vector_b.push_back("b");
-			vector_b.push_back("c");
-			vector_b.push_back("d");
-			vector_b.push_back("e");
-
-			std::chrono::system_clock::time_point time_point_a(std::chrono::system_clock::now());
-
-			my::misc::sleep_milliseconds(1);
-
-			std::chrono::system_clock::time_point time_point_b(std::chrono::system_clock::now());
+			std::vector<std::string const> vector_a(new_vector());
+			std::vector<std::string const> vector_b(new_vector());
 
 			my::misc::shuffle(time_point_a, vector_a);
 			my::misc::shuffle(time_point_a, vector_b);
 
-			Assert::IsTrue(
-				vector_a[0] == vector_b[0]
-				&& vector_a[1] == vector_b[1]
-				&& vector_a[2] == vector_b[2]
-				&& vector_a[3] == vector_b[3]
-				&& vector_a[4] == vector_b[4]);
+			assert_vectors(vector_a, vector_b, true);
+		}
+
+		for (unsigned int i(0); i < 3; ++i)
+		{
+			std::vector<std::string const> vector_a(new_vector());
+			std::vector<std::string const> vector_b(new_vector());
 
 			my::misc::shuffle(time_point_a, vector_a);
 			my::misc::shuffle(time_point_b, vector_b);
 
-			Assert::IsFalse(
-				vector_a[0] == vector_b[0]
-				&& vector_a[1] == vector_b[1]
-				&& vector_a[2] == vector_b[2]
-				&& vector_a[3] == vector_b[3]
-				&& vector_a[4] == vector_b[4]);
+			assert_vectors(vector_a, vector_b, false);
 		}
+	}
 };
